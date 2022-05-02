3cb80860673ffcd7411c5ee1f7cb8963b4e55d06
Kha Tran <khatran961217@gmail.com>
2022-05-02 13:19:01 +0700

    Initial Commit

---
+++ Weatherly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weatherly
+//
+//  Created by Kha Tran on 2/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Kha Tran on 2/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
